02b5a353a706ab0a40c69c5d8c185cae923e9fc4
Kevin Luis <kevluis10@gmail.com>
2023-02-15 23:09:44 +0800

    MVP done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 
 import styles from "@/styles/Home.module.css";
+
 import "bootstrap/dist/css/bootstrap.css";
-import Image from "next/image";
 import Link from "next/link";
+import useDebounce from "@/hooks/useDebounce";
 
 interface HeroResponse {
   results: [];
@@ -19,105 +20,132 @@ interface Hero {
 
 const Home = () => {
   const [heroName, setHeroName] = useState("");
-  const [heroSelected, setHeroSelected] = useState("");
   const [heroList, setHeroList] = useState([]);
   const [realTime, setRealTime] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [searching, setSearching] = useState(false);
+
+  const debounceSearch = useDebounce(heroName, 500);
 
   useEffect(() => {
-    if (realTime) {
+    if (realTime && debounceSearch === heroName) {
       filterHeroes();
     }
-  }, [heroName, realTime]);
+  }, [heroName, realTime, debounceSearch]);
+
+  useEffect(() => {
+    const { Modal } = require("bootstrap");
+    const myModal = new Modal("#heroSelectModal");
+    if (loading) {
+      myModal.show();
+    }
+    return () => {
+      myModal.hide();
+    };
+  }, [loading]);
 
   const filterHeroes = async () => {
+    setSearching(true);
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_SUPERHEROES_BASE_URL}${process.env.NEXT_PUBLIC_FB_ACCESS_TOKEN}/search/${heroName}`
       );
       const data: HeroResponse = await response.json();
       setHeroList(data.results);
+      setSearching(false);
     } catch (err) {
       console.log(err);
     }
   };
 
-  const selectHero = (id: string) => {
-    setLoading(true);
-    setHeroSelected(id);
-  };
-
   return (
-    <div className="d-flex">
-      <div className="mx-auto my-3 w-50">
-        <div className={styles.searchBar}>
-          <input
-            className="w-50"
-            value={heroName}
-            placeholder="Search your superhero here..."
-            onChange={(event) => setHeroName(event.target.value)}
-          />
-          {!realTime && (
-            <button onClick={filterHeroes} className="mx-1">
-              Search
-            </button>
-          )}
-          <div className="mt-1">
-            <input
-              type="checkbox"
-              checked={realTime}
-              onClick={(event) => {
-                return setRealTime(event.currentTarget.checked);
-              }}
-            />
-            <span className="mx-2">real-time filtering</span>
+    <>
+      <div
+        className="modal fade"
+        id="heroSelectModal"
+        tabIndex={-1}
+        aria-labelledby="heroSelectModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="heroSelectModalLabel">
+                Loading Character Info...
+              </h5>
+            </div>
+            <div className="d-flex">
+              <div className="spinner-border mx-auto my-5" role="status">
+                <span className="sr-only"></span>
+              </div>
+            </div>
           </div>
         </div>
-        <div>
-          <ul className="list-group mt-3">
-            {heroList?.length > 0 ? (
-              heroList.map((hero: Hero) => {
-                return (
-                  <Link
-                    className="text-decoration-none"
-                    href={`/profile/${encodeURIComponent(hero.id)}`}
-                    key={hero.id}
-                    onClick={() => selectHero(hero.id)}
-                  >
-                    <li className={`list-group-item ${styles.item}`}>
-                      <img
-                        src={hero.image.url}
-                        alt="avatar"
-                        className={`mx-3 ${styles.avatar}`}
-                      ></img>
-                      {hero.name}
-                      {heroSelected === hero.name && (
-                        <div className="d-flex justify-content-center">
-                          <div className="spinner-border" role="status">
-                            <span className="sr-only">Loading...</span>
-                          </div>
-                        </div>
-                      )}
-                    </li>
-                  </Link>
-                );
-              })
-            ) : (
-              <li
-                className={`list-group-item text-center fs-5 ${styles.empty}`}
-              >
-                <Image
-                  src="/hulk.png"
-                  alt="hulk search"
-                  width="500"
-                  height="500"
-                />
-              </li>
+      </div>
+
+      <div className="d-flex">
+        <div className="mx-auto my-3 w-50">
+          <div className={styles.searchBar}>
+            <input
+              className="w-50"
+              value={heroName}
+              placeholder="Search your superhero here..."
+              onChange={(event) => setHeroName(event.target.value)}
+            />
+            {!realTime && (
+              <button onClick={filterHeroes} disabled={searching} className="mx-1 rounded">
+                {searching ?
+                <div className="spinner-grow spinner-grow-sm mx-3" role="status">
+                  <span className="sr-only"></span>
+                </div> : "Search"
+                }
+              </button>
             )}
-          </ul>
+            <div className="mt-1">
+              <input
+                type="checkbox"
+                checked={realTime}
+                onClick={(event) => {
+                  return setRealTime(event.currentTarget.checked);
+                }}
+              />
+              <span className="mx-2">real-time filtering</span>
+            </div>
+          </div>
+          <div>
+            <ul className="list-group mt-3">
+              {heroList?.length > 0 ? (
+                heroList.map((hero: Hero) => {
+                  return (
+                    <Link
+                      className="text-decoration-none"
+                      href={`/profile/${encodeURIComponent(hero.id)}`}
+                      key={hero.id}
+                      onClick={() => setLoading(true)}
+                    >
+                      <li className={`list-group-item ${styles.item}`}>
+                        <img
+                          src={hero.image.url}
+                          alt="avatar"
+                          className={`mx-3 ${styles.avatar}`}
+                        ></img>
+                        {hero.name}
+                      </li>
+                    </Link>
+                  );
+                })
+              ) : (
+                <li
+                  className={`list-group-item text-center fs-5 ${styles.empty}`}
+                >
+                  <img src="/hulk.png" alt="hulk search" className="w-100" />
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/profile/[id]/index.tsx
+++ src/pages/profile/[id]/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { useRouter } from "next/router";
+import { BsCaretLeftFill } from "react-icons/bs";
+import styles from "@/styles/Profile.module.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Link from "next/link";
 
 interface PowerStats {
   intelligence?: string;
@@ -34,11 +37,70 @@ interface HeroResponse {
   results: [];
 }
 
-const ProfilePage: NextPage<HeroInfo> = (props) => {
-  return <>the name: {props.name}</>;
+const Profile: NextPage<HeroInfo> = ({
+  id,
+  name,
+  powerstats,
+  biography,
+  image,
+}) => {
+  return (
+    <>
+      <div
+        className={`${styles.container} ${
+          !!biography?.alignment && styles[biography.alignment]
+        } d-flex`}
+      >
+        <Link href="/" className={`text-decoration-none ${biography?.alignment === "good" ? styles.backGood : styles.backBad}`}>
+          <BsCaretLeftFill />
+          Back
+        </Link>
+      </div>
+      <div
+        className={`${styles.container} ${
+          !!biography?.alignment && styles[biography.alignment]
+        } d-flex`}
+      >
+        <img
+          src={image?.url}
+          alt="Profile Picture"
+          className={`mx-3 ${styles.avatar}`}
+        ></img>
+        <div className="d-grid">
+          <span className={`mx-5 ${styles.heroName}`}>
+            {name?.toUpperCase() ?? ''}
+          </span>
+          <span className="mx-5">
+            Secret Identity:{" "}
+            {!!biography && !!biography["full-name"]
+              ? biography["full-name"]
+              : name}
+          </span>
+          <span className="mx-5">
+            A.K.A: {(!!biography && biography.aliases?.join(", ")) ?? "N/A"}
+          </span>
+          <span className="mx-5">
+            Origin: {(!!biography && biography["place-of-birth"]) ?? "Unkown"}
+          </span>
+        </div>
+      </div>
+      <div
+        className={`${styles.container} ${
+          !!biography?.alignment && styles[biography.alignment]
+        } d-flex justify-content-between`}
+      >
+        <span>Combat: {powerstats?.combat}</span>
+        <span>Durability: {powerstats?.durability}</span>
+        <span>Intelligence: {powerstats?.intelligence}</span>
+        <span>Power: {powerstats?.power}</span>
+        <span>Speed: {powerstats?.speed}</span>
+        <span>Strength: {powerstats?.strength}</span>
+      </div>
+    </>
+  );
 };
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_SUPERHEROES_BASE_URL}${process.env.NEXT_PUBLIC_FB_ACCESS_TOKEN}/${params?.id}`
   );
@@ -66,4 +128,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export default ProfilePage;
+export default Profile;

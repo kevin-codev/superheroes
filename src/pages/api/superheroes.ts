b6e78d44c233232c8edff9360ef9c72fc3cb111a
Kevin Luis <kevluis10@gmail.com>
2023-02-16 23:13:03 +0800

    prettified

--- src/pages/api/superheroes.ts
+++ src/pages/api/superheroes.ts
@@ -1,19 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  results: []
-}
+  results: [];
+};
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) => {
-  if (req.method === 'GET') {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SUPERHEROES_BASE_URL}${process.env.NEXT_PUBLIC_FB_ACCESS_TOKEN}/search/${req.query.heroName}`);
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  if (req.method === "GET") {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SUPERHEROES_BASE_URL}${process.env.NEXT_PUBLIC_FB_ACCESS_TOKEN}/search/${req.query.heroName}`
+    );
     const data: Data = await response.json();
-    return res.status(200).json(data)
+    return res.status(200).json(data);
   }
   return res.status(400);
-}
+};
 
 export default handler;
